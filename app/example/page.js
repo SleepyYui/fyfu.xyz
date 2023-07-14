7c4a2f9d72c989a6f5c239defda9411f63ddf1cf
Faye <dominic@stilma.net>
2023-07-14 12:00:47 +0200

    init

---
+++ app/example/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Example() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl">This is an example! </h1>
+	  <span>This is how you can make subdirectories!</span>
+    </main>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+	  colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'black': '#000',
+        'white': '#fff',
+        'blurple': '#5865F2',
+        'darkwhite': '#424549',
+      },
+    },
+  },
+  plugins: [],
+}
